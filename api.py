7b856c0569c688ca7aa785baf17cb6860abaa0b3
William Mitchell <wfmitch@me.com>
2020-06-02 11:54:28 -0400

    updated venv to add .venv

--- api.py
+++ api.py
@@ -1,17 +1,52 @@
 import logging as log
 import sys
-sys.path.append('/users/wfmit/onedrive/documents/github/stockwits')
-from requestors import ST_BASE_PARAMS, ST_BASE_URL
+import os
+import requests 
+import urllib
+import json
 
-
-# Select which library to use for handling HTTP request.  If running on Google App Engine, use `GAE`.
-# Otherwise, use `Requests` which is based on the `requests` module.
-from requestors import Requests as R
-
-__author__ = 'Jason Haury'
+RC = 0
 
 # Example list of exchanges to limit a watchlist to
 EXCHANGES = ['NYSE', 'NASDAQ', 'NYSEMkt', 'NYSEArca']
+
+# StockTwits details
+ST_BASE_URL = 'https://api.stocktwits.com/api/2/'
+ST_BASE_PARAMS = dict(access_token=os.getenv('ST_ACCESS_TOKEN'))
+
+def get_json(self,url, params=None):
+    """ Uses tries to GET a few times before giving up if a timeout.  returns JSON
+    """
+    resp = None
+    for i in range(4):
+        try:
+            resp = requests.get(url, params=params, timeout=5)
+        except requests.Timeout:
+            trimmed_params = {k: v for k, v in params.iteritems() if k not in ST_BASE_PARAMS.keys()}
+            log.error('GET Timeout to {} w/ {}'.format(url[len(ST_BASE_URL):], trimmed_params))
+        if resp is not None:
+            break
+    if resp is None:
+        log.error('GET loop Timeout')
+        return None
+    else:
+        return json.loads(resp.content)
+
+def post_json(self,url, params=None, deadline=30):
+    """ Tries to post a couple times in a loop before giving up if a timeout.
+    """
+    resp = None
+    for i in range(4):
+        try:
+            resp = requests.post(url, params=params, timeout=5)
+        except requests.Timeout:
+            trimmed_params = {k: v for k, v in params.iteritems() if k not in ST_BASE_PARAMS.keys()}
+            log.error('POST Timeout to {} w/ {}'.format(url[len(ST_BASE_URL):], trimmed_params))
+        if resp is not None:
+            break
+    # TODO wrap in appropriate try/except
+    return json.loads(resp.content)
+
 
 
 
@@ -83,7 +118,7 @@ def clean_watchlist(wl_id):
     """ Deletes stocks to follow if they aren't part of NASDAQ or NYSE
     """
     wl = R.get_json(ST_BASE_URL + 'watchlists/show/{}.json'.format(wl_id),
-                  params=ST_BASE_PARAMS)['watchlist']['symbols']
+                params=ST_BASE_PARAMS)['watchlist']['symbols']
     qty_deleted = 0
     for sym in wl:
         if sym['exchange'] not in EXCHANGES:
@@ -93,3 +128,11 @@ def clean_watchlist(wl_id):
             else:
                 log.error("Error deleting symbol from watchlist: {}".format(sym))
     return qty_deleted
+
+def main(): 
+
+
+    sys.exit(RC)
+
+if __name__=='__main__':
+    main()  
---
+++ urls.py
@@ -0,0 +1,14 @@
+# https://api.stocktwits.com/api/2/oauth/authorize #oauth/authorize
+
+# https://api.stocktwits.com/api/2/oauth/token #oauth/token
+
+# https://api.stocktwits.com/api/2/streams/user/:id.json #streams/user
+
+# https://api.stocktwits.com/api/2/streams/symbol/:id.json
+# https://api.stocktwits.com/api/2/streams/symbol/ric/:id.json #streams/symbol
+
+# https://api.stocktwits.com/api/2/streams/trending.json #streams/trending
+
+# https://api.stocktwits.com/api/2/trending/symbols/equities.json #trending/symbols/equities
+
+# https://api.stocktwits.com/api/2/trending/symbols.json #trending/symbols
